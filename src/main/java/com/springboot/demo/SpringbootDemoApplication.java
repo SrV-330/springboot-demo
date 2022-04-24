551f598692dbf09081d36aab64378210c3ba5ac3
SrV <985693016@qq.com>
2022-04-24 17:35:55 +0800

    msg

--- src/main/java/com/springboot/demo/SpringbootDemoApplication.java
+++ src/main/java/com/springboot/demo/SpringbootDemoApplication.java
@@ -7,9 +7,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
+@EnableEurekaClient
 @Slf4j
 public class SpringbootDemoApplication {
 
