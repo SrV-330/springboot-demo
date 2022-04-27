4ce31c4dcbd3796fbfe65f42b0549d214ecff9e6
SrV <985693016@qq.com>
2022-04-27 23:30:59 +0800

    msg

--- src/main/java/com/springboot/demo/process/TestProcess.java
+++ src/main/java/com/springboot/demo/process/TestProcess.java
@@ -1,22 +1,46 @@
 package com.springboot.demo.process;
 
+import com.google.auto.service.AutoService;
 import com.springboot.demo.annotation.TestAnno;
+import com.sun.tools.javac.api.JavacTrees;
+import com.sun.tools.javac.processing.JavacProcessingEnvironment;
+import com.sun.tools.javac.tree.TreeMaker;
+import com.sun.tools.javac.util.Context;
+import com.sun.tools.javac.util.Names;
 
-import javax.annotation.processing.AbstractProcessor;
-import javax.annotation.processing.RoundEnvironment;
-import javax.annotation.processing.SupportedAnnotationTypes;
-import javax.annotation.processing.SupportedSourceVersion;
+import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 import java.util.Set;
 
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
 @SupportedAnnotationTypes("com.springboot.demo.annotation.TestAnno")
+@AutoService(Processor.class)
 public class TestProcess extends AbstractProcessor {
+
+    private Messager messager;
+    private JavacTrees javacTrees;
+    private TreeMaker treeMaker;
+    private Names names;
+
+    @Override
+    public synchronized void init(ProcessingEnvironment processingEnv) {
+        super.init(processingEnv);
+        this.messager = processingEnv.getMessager();
+        this.javacTrees = JavacTrees.instance(processingEnv);
+        Context context = ((JavacProcessingEnvironment) processingEnv).getContext();
+        treeMaker = TreeMaker.instance(context);
+        names = Names.instance(context);
+    }
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        roundEnv.getElementsAnnotatedWith(TestAnno.class);
+        Set<? extends Element> eleAnnoSet = roundEnv.getElementsAnnotatedWith(TestAnno.class);
+        eleAnnoSet.forEach(element -> {
+
+        });
+
         return true;
     }
 }
